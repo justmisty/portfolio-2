3252c99ff67818a840f986660aa68310caf9b56e
Kidochukwu Anyasi <anyasibright@gmail.com>
2024-07-24 19:05:49 +0100

    added project: expense tracker

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import styles from "./ProjectStyles.module.css";
 import cryptoPlace from "../../assets/crypto.png";
 import netflix from "../../assets/netflix.png";
 import giphy from "../../assets/giphy.png";
+import expense from '../../assets/expense.png'
 import spotify from "../../assets/spotify.png";
 import carePulse from "../../assets/carepulse1.png";
 import ProjectCard from "../../common/ProjectCard";
@@ -24,6 +25,12 @@ const Projects = () => {
           link="https://redtube1.netlify.app/"
           title="Netflix"
           description="Streaming service"
+        />
+        <ProjectCard
+          src={expense}
+          link="https://uexpense.netlify.app/"
+          title="Expense Tracker"
+          description="Manage Expenses"
         />
         <ProjectCard
           src={giphy}
